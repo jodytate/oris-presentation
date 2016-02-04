a34fb691cb5d13b7bf6a9b76871237d459d94d41
jody <jtate@uw.edu>
2016-02-04 07:27:36 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// $ : use jQuery, a JavaScript helper library
+// #the-button : to get the button element
+// .click : and when i click it
+// #the-paragraph : get the paragraph
+// .toggle : and toggle it's visibility
+
+$('#the-button').click(function(){
+  $('#the-paragraph').toggle();
+});
